3330a82bc3f60a30c117a1b98bc3ed2d6ee85a5a
Glitch (hello-express) <none>
2021-01-09 16:55:55 +0000

    🌇🚖 Checkpoint ./watch.json:27148953/16 ./index.js:27148953/152

--- index.js
+++ index.js
@@ -12,4 +12,9 @@ const Discord = require("discord.js"); //Conexão com a livraria Discord.js
 const client = new Discord.Client(); //Criação de um novo Client
 const config = require("./config.json"); //Pegando o prefixo do bot para respostas de comandos
 
+client.on("message", async message => {
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+} )
+
 client.login(process.env.TOKEN); //Ligando o Bot caso ele consiga acessar o token
